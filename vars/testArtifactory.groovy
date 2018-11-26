63dce4687a519478ddf94a6eef8dc78c817a66e8
mackah666 <mackah666@yahoo.co.uk>
2018-11-26 05:46:38 +0000

    Changed quote types

--- vars/testArtifactory.groovy
+++ vars/testArtifactory.groovy
@@ -1,9 +1,6 @@
 def call(String serveruri, String username, String password) {
 
-    def server = Artifactory.newServer url: '${serveruri}', username: '${username}', password: '${password}'
+    def server = Artifactory.newServer url: "${serveruri}", username: "${username}", password: "${password}"
 
-    echo '${serveruri} ${username} ${password}'
-
-    
-    
+    echo "${serveruri} ${username} ${password}"
 }
