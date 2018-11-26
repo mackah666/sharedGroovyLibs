8cdf674ebf4a01a68722d179255ce009420bfa88
mackah666 <mackah666@yahoo.co.uk>
2018-11-26 06:07:03 +0000

    Fixing string deferencing

--- vars/testArtifactory.groovy
+++ vars/testArtifactory.groovy
@@ -1,5 +1,5 @@
 def call(String serveruri, String username, String password) {
 
     def server = Artifactory.newServer url: '${serveruri}', username: '${username}', password: '${password}'
-    echo ${serveruri} ${username} ${password}
+    echo "Theses are the values ${serveruri} ${username} ${password}"
 }
